feeab62d6bf199994d90d8fcbc705c6558f2dbed
burentugs <delgercecegbrentgs@gmail.com>
2025-12-09 22:27:24 +0800

    Maven-based Java project initial structure added

---
+++ calculator/src/main/java/lab14/sict/must/edu/mn/App.java
@@ -0,0 +1,13 @@
+package lab14.sict.must.edu.mn;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
